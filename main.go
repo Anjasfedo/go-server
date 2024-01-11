b687cc0ceaf66efc9e91aea39f35a2417f27080f
AnjasFedo <fedoaf98@gmail.com>
2024-01-11 18:50:59 +0700

    / and /hello Handler

--- main.go
+++ main.go
@@ -1,11 +1,37 @@
 package main
 
-import(
+import (
 	"fmt"
 	"log"
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "hello!")
+}
+
 func main() {
-	
+	fileserver := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileserver)
+
+	http.HandleFunc("/hello", helloHandler)
+
+	// http.HandleFunc("/form", formHandler)
+
+	fmt.Printf("Starting server at http://localhost:8080\n")
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
